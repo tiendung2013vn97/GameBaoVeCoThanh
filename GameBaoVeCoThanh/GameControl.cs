f568c3a21aa653e2183fccf5c3b22f1fcad3afea
ngophuochai <ngophuochai1512138@gmail.com>
2019-01-03 01:18:36 +0700

    add gamecontrol

---
+++ GameBaoVeCoThanh/GameControl.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBaoVeCoThanh
+{
+    static class GameControl
+    {
+        public static Game g = null;
+        public static SpriteBatch sb = null;
+        public static GameTime gt = null;
+    }
+}
